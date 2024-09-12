12f3f501a827fa2e9137a0f0e591aacdf35c7934
DanielTirado <danieltirado088@gmail.com>
2024-09-12 09:07:42 -0500

    Mostrar voltaje en pantalla LCD

--- ArduinoSenalesTinkercad.ino
+++ ArduinoSenalesTinkercad.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+// C++ code
+//
+#include <Adafruit_LiquidCrystal.h>
 
+int analogPin = 0;
+int val = 0;
+
+Adafruit_LiquidCrystal lcd_1(0);
+
+void setup()
+{
+  lcd_1.begin(16, 2);
+  Serial.begin(9600);
+  lcd_1.clear();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  lcd_1.setCursor(0, 0);
+  val = analogRead(analogPin);
+  lcd_1.print(val);
+  delay(500); // Wait for 500 millisecond(s)
+  Serial.println(val);
+  lcd_1.clear();
 }
