4dad268759e87238751e8fb79cc9bcdb55a96ff8
DanielTirado <danieltirado088@gmail.com>
2024-09-13 17:19:18 -0500

    Montaje en 1 solo arduino

--- ArduinoSenalesTinkercad.ino
+++ ArduinoSenalesTinkercad.ino
@@ -1,25 +1,49 @@
-// C++ code
-//
+/*
+Montaje con dos pulsadores: uno para iniciar la muestra de datos y 
+otro para detenerla. En realidad, un pulsador en alto permite que se 
+haga la lectura del pin analógico, mientras que el otro pulsador en 
+alto le asigna un valor de 0 a este pin.
+
+Próximamente se modificará el montaje de tal manera que un pulsador 
+solicite la información de la señal capturada (frecuencia y amplitud) 
+calculada con algún algoritmo.
+
+*/
 #include <Adafruit_LiquidCrystal.h>
-
-int analogPin = 0;
-int val = 0;
-
 Adafruit_LiquidCrystal lcd_1(0);
+
+int val = 0;
+int analogPin = 0;
+int button1Pin = 2;
+int button2Pin = 4;
+bool startData = false;
 
 void setup()
 {
-  lcd_1.begin(16, 2);
+  pinMode(2, INPUT);
+  pinMode(4, INPUT);
   Serial.begin(9600);
-  lcd_1.clear();
+  lcd_1.begin(16, 2);
 }
 
 void loop()
-{
-  lcd_1.setCursor(0, 0);
-  val = analogRead(analogPin);
+{ 
+  lcd_1.setCursor(2, 1);
+  if(digitalRead(button1Pin) == HIGH) {
+    startData = true;
+  }
+  if(digitalRead(button2Pin) == HIGH) {
+    startData = false;
+  }
+  
+  if(startData) { 
+    val = analogRead(analogPin);
+  }
+  else{
+    val = 0;
+  }
   lcd_1.print(val);
-  delay(500); // Wait for 500 millisecond(s)
+  delay(10);
   Serial.println(val);
   lcd_1.clear();
 }
